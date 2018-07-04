a68c66b35d422efd64c8a91a46d545fa3642170c
KOLANICH <kolan_n@mail.ru>
2018-07-04 18:26:50 +0300

    Some fixes * Added CMake build * Added some macros to build for Windows. It builds, but crashes. Under debugger it partially works, but produces nonsense. * Some style fixes * Removed the dependencies from the repo. * Added .editorconfig * Added resource generation though it isn't shown by Windows because of some reason.

---
+++ cmake/FindFuse.cmake
@@ -0,0 +1,35 @@
+# Find the FUSE includes and library
+#
+#  FUSE_INCLUDE_DIR - where to find fuse.h, etc.
+#  FUSE_LIBRARIES   - List of libraries when using FUSE.
+#  FUSE_FOUND       - True if FUSE lib is found.
+
+# check if already in cache, be silent
+if (FUSE_INCLUDE_DIR)
+        SET (FUSE_FIND_QUIETLY TRUE)
+endif (FUSE_INCLUDE_DIR)
+
+if (APPLE)
+    set (FUSE_NAMES libosxfuse.dylib fuse)
+    set (FUSE_SUFFIXES osxfuse fuse)
+elseif(WINDOWS)
+    set (FUSE_NAMES libdokanfuse1)
+    set (FUSE_SUFFIXES dokanfuse1)
+else()
+    set(FUSE_NAMES fuse)
+    set(FUSE_SUFFIXES fuse)
+endif()
+
+# find includes
+find_path (FUSE_INCLUDE_DIR fuse.h
+        PATHS /opt /opt/local /usr/pkg
+        PATH_SUFFIXES ${FUSE_SUFFIXES})
+
+# find lib
+find_library (FUSE_LIBRARIES NAMES ${FUSE_NAMES})
+
+include ("FindPackageHandleStandardArgs")
+find_package_handle_standard_args ("FUSE" DEFAULT_MSG
+    FUSE_INCLUDE_DIR FUSE_LIBRARIES)
+
+mark_as_advanced (FUSE_INCLUDE_DIR FUSE_LIBRARIES)
--- src/fuse7z.h
+++ src/fuse7z.h
@@ -28,132 +28,15 @@ class Fuse7z
 	Fuse7zInStream stream;
 
 	public:
-	Fuse7z(std::string const & filename, std::string const & cwd) :
-         stream (filename),
-         archive_fn (filename),
-         cwd (cwd)
-    {
-	    root_node = new Node(NULL, "");
-    	root_node->is_dir = true;
+	Fuse7z(std::string const & filename, std::string const & cwd);
 
-        Logger &logger = Logger::instance ();
-		logger << "Initialization of fuse-7z with archive " << filename << Logger::endl;
+	virtual ~Fuse7z();
 
-		if (!lib.Initialize()) {
-			throw std::runtime_error("7z library initialization failed. Is the 7z.so folder in LD_LIBRARY_PATH?");
-		}
+	virtual void open(char const * path, Node * node);
 
-		WStringArray exts;
+	virtual void close(char const * path, Node * node);
 
-		if (!lib.GetSupportedExts(exts)) {
-			std::stringstream ss;
-			ss << "Could not get list of 7z-supported file extensions";
-			throw std::runtime_error(ss.str());
-		}
-
-
-		logger << "Supported extensions : ";
-		size_t size = exts.size();
-		for(size_t i = 0; i < size; i++) {
-			std::wstring ext = exts[i];
-
-
-			for(size_t j = 0; j < ext.size(); j++) {
-				logger << (char)(ext[j] &0xFF);
-			}
-			logger << " ";
-
-		}
-		logger << Logger::endl;
-
-
-		if (lib.OpenArchive(&stream, &archive)) {
-			unsigned int numItems = 0;
-
-			archive->GetItemCount(&numItems);
-
-			logger << "Archive contains " << numItems << " entries" << Logger::endl;
-			Node * node = 0;
-			for(unsigned int i = 0;i < numItems;i++) {
-				C7ZipArchiveItem * pArchiveItem = NULL;
-				if (archive->GetItemInfo(i, &pArchiveItem)) {
-					std::wstring wpath(pArchiveItem->GetFullPath());
-					std::string path;
-					path.resize(wpath.length());
-					std::copy(wpath.begin(), wpath.end(), path.begin());
-
-					node = root_node->insert(const_cast<char*>(path.c_str()));
-					node->id = i;
-
-					node->is_dir = pArchiveItem->IsDir();
-
-					{
-						unsigned long long size;
-						pArchiveItem->GetUInt64Property(lib7zip::kpidSize, size);
-						node->stat.st_size = size;
-					}
-
-					{
-						unsigned long long secpy, time, bias, gain;
-						secpy = 31536000;
-						gain = 10000000ULL;
-						bias = secpy * gain * 369 + secpy * 2438356ULL + 5184000ULL;
-						pArchiveItem->GetFileTimeProperty(lib7zip::kpidATime, time);
-						node->stat.st_atim.tv_sec = (time - bias)/gain;
-						node->stat.st_atim.tv_nsec = ((time - bias) % gain) * 100;
-						pArchiveItem->GetFileTimeProperty(lib7zip::kpidCTime, time);
-						node->stat.st_ctim.tv_sec = (time - bias)/gain;
-						node->stat.st_ctim.tv_nsec = ((time - bias) % gain) * 100;
-						pArchiveItem->GetFileTimeProperty(lib7zip::kpidMTime, time);
-						node->stat.st_mtim.tv_sec = (time - bias)/gain;
-						node->stat.st_mtim.tv_nsec = ((time - bias) % gain) * 100;
-					}
-				}
-
-				if (node && ((i+1) % 10000 == 0)) {
-					logger << "Indexed " << (i+1) << "th file : " << node->fullname() << Logger::endl;
-				}
-			}
-		}
-		else {
-			std::stringstream ss;
-			ss << "open archive " << archive_fn  << " failed" << std::endl;
-			throw std::runtime_error(ss.str());
-		}
-	}
-
-	virtual ~Fuse7z() {
-		delete archive;
-		lib.Deinitialize();
-
-        delete root_node;
-	}
-
-	virtual void open(char const * path, Node * node) {
-        Logger &logger = Logger::instance ();
-		logger << "Opening file " << path << "(" << node->fullname() << ")" << Logger::endl;
-		Fuse7zOutStream * stream = new Fuse7zOutStream;
-		node->buffer = stream;
-		stream->SetSize(node->stat.st_size);
-		int id = node->id;
-		archive->Extract(id, stream);
-	}
-
-	virtual void close(char const * path, Node * node) {
-        Logger &logger = Logger::instance ();
-		logger << "Closing file " << path << "(" << node->fullname() << ")" << Logger::endl;
-		Fuse7zOutStream * stream = dynamic_cast<Fuse7zOutStream*>(node->buffer);
-		delete stream;
-		node->buffer = NULL;
-	}
-
-	virtual int read(char const * path, Node * node, char * buf, size_t size, off_t offset) {
-        Logger &logger = Logger::instance ();
-		logger << "Reading file " << path << "(" << node->fullname() << ") for " << size << " at " << offset << ", arch_id=" << node->id << Logger::endl;
-		Fuse7zOutStream * stream = dynamic_cast<Fuse7zOutStream*>(node->buffer);
-		memcpy(buf, &stream->buffer[offset], size);
-		return size;
-	}
+	virtual int read(char const * path, Node * node, char * buf, size_t size, off_t offset);
 
     // FIXME: these must be private
     public:
